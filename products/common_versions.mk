374182c3fecc00d62927d90223a9d5c6ed455416
Chris Phelps <cphelps76@cphelps76-HP-Pavilion-dv7.(none)>
2012-12-23 00:30:03 -0600

    add commented out item for allowing Developer Id overrides

--- products/common_versions.mk
+++ products/common_versions.mk
@@ -5,8 +5,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=JOP40D BUILD_VERSION_TAGS=relea
 # Additional properties
 PRODUCT_VERSION_MAJOR = 10
 PRODUCT_VERSION_MINOR = 0
-
+ 
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.modversion=DEMENTED-Droid-$(shell date +"%m-%d-%y")
-    
+ro.modversion=DEMENTED-Droid-$(shell date +"%m-%d-%y") 
 
+# Uncomment the below lines and insert name to be displayed as Developer Id
+# PRODUCT_BUILD_PROP_OVERRIDES += \
+# ro.build.host=
