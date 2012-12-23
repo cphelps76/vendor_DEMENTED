bf4999c926376a636ed316dac90662b64cbf10ff
Chris Phelps <cphelps76@cphelps76-HP-Pavilion-dv7.(none)>
2012-12-23 01:20:25 -0600

    comment revised

--- products/common_versions.mk
+++ products/common_versions.mk
@@ -10,5 +10,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ro.modversion=DEMENTED-Droid-$(shell date +"%m-%d-%y") 
 
 # Uncomment the below lines and insert name to be displayed as Developer Id
+# Host Build Machine is displayed by default
 # PRODUCT_BUILD_PROP_OVERRIDES += \
 # ro.build.host=
