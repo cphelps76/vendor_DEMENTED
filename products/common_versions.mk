4d3a9fe3684fae25f8b03aa4a5970dc5c37b512b
Chris Phelps <cphelps76@cphelps76-HP-Pavilion-dv7.(none)>
2012-12-24 02:36:55 -0600

    dev id

--- products/common_versions.mk
+++ products/common_versions.mk
@@ -1,5 +1,5 @@
 # Version information used on all builds
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=JOP40D BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=JOP40D BUILD_VERSION_TAGS=release-keys USER=cphelps76 BUILD_UTC_DATE=$(shell date +"%s")
 
 
 # Additional properties
@@ -7,9 +7,4 @@ PRODUCT_VERSION_MAJOR = 10
 PRODUCT_VERSION_MINOR = 0
  
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.modversion=DEMENTED-Droid-$(shell date +"%m-%d-%y") 
-
-# Uncomment the below lines and insert name to be displayed as Developer Id
-# Host Build Machine is displayed by default
-# PRODUCT_BUILD_PROP_OVERRIDES += \
-# ro.build.host=
+ro.modversion=DEMENTED-Droid-$(shell date +"%m-%d-%y")
