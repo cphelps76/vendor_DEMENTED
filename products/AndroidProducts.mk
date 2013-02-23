ee3aa0357c710af72faaeada9811bd09987ba0ea
Chris Phelps <cphelps76@chris-HP-Pavilion-dv7.(none)>
2013-02-23 12:20:36 -0600

    Revert "moto bs"
    This reverts commit 43adbfe35214eeb3b9153e7147c9d2c656739d42.

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -2,5 +2,5 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/maguro.mk \
     $(LOCAL_DIR)/toro.mk \
     $(LOCAL_DIR)/mako.mk \
-    $(LOCAL_DIR)/grouper.mk \
-    $(LOCAL_DIR)/spyder.mk
+    $(LOCAL_DIR)/grouper.mk
+
