d96649ee1b6bdb2d296c84795c9fd04d99c85736
cphelps76 <cphelps76@gmail.com>
2013-03-05 12:44:41 -0600

    speed_freek update

--- products/speed_freek.mk
+++ products/speed_freek.mk
@@ -40,14 +40,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # init.d support
 PRODUCT_COPY_FILES += \
+    vendor/DEMENTED-Droid/proprietary/common/etc/init.d/01dementedbomb:system/etc/init.d/01dementedbomb \
     vendor/DEMENTED-Droid/proprietary/common/etc/init.d/02dementedkernel:system/etc/init.d/02dementedkernel \
     vendor/DEMENTED-Droid/proprietary/common/etc/init.d/02sysctl:system/etc/init.d/02sysctl \
-    vendor/DEMENTED-Droid/proprietary/common/etc/init.d/03dementedgov:system/etc/init.d/03dementedgov \
     vendor/DEMENTED-Droid/proprietary/common/etc/init.d/04dementededt_tweaks:system/etc/init.d/04dementededt_tweaks \
     vendor/DEMENTED-Droid/proprietary/common/etc/init.d/05dementedrfs:system/etc/init.d/05dementedrfs \
     vendor/DEMENTED-Droid/proprietary/common/etc/init.d/07dementedtweaks:system/etc/init.d/07dementedtweaks \
     vendor/DEMENTED-Droid/proprietary/common/etc/init.d/09dementedsetrenice:system/etc/init.d/09dementedsetrenice \
     vendor/DEMENTED-Droid/proprietary/common/etc/init.d/80dementedloopysmoothnesstweak:system/etc/init.d/80dementedloopysmoothnesstweak \
     vendor/DEMENTED-Droid/proprietary/common/etc/init.d/91dementedzipalign:system/etc/init.d/91dementedzipalign \
+    vendor/DEMENTED-Droid/proprietary/common/etc/init.d/97dementedtcp:system/etc/init.d/97dementedtcp \
     vendor/DEMENTED-Droid/proprietary/common/etc/init.d/98tweaks:system/etc/init.d/98tweaks \
     vendor/DEMENTED-Droid/proprietary/common/etc/init.d/99dementedsystem:system/etc/init.d/99dementedsystem 
