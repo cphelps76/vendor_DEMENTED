80ee01414da288e009a1316bb4d2997196a3f15d
cphelps76 <cphelps76@gmail.com>
2013-05-02 10:40:19 -0500

    new init.d scripts

--- products/grouper.mk
+++ products/grouper.mk
@@ -20,7 +20,7 @@ PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=nakasi BUILD_FINGERPRINT=google/nak
 -include vendor/DEMENTED-Droid/products/common_versions.mk
 
 # DEMENTED-Droid SPEED FREEK init.d and build.prop tweaks
-include vendor/DEMENTED-Droid/products/speed_freek_grouper.mk
+include vendor/DEMENTED-Droid/products/speed_freek.mk
 
 # Additional DEMENTED-Droid Apps
 PRODUCT_PACKAGES += \
