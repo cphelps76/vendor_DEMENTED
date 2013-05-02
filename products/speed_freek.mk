4d89d96af52a330c2fd77be5632e1bf04561554d
cphelps76 <cphelps76@gmail.com>
2013-05-02 11:10:01 -0500

    derp

--- products/speed_freek.mk
+++ products/speed_freek.mk
@@ -4,6 +4,6 @@
 PRODUCT_COPY_FILES += \
     vendor/DEMENTED-Droid/proprietary/common/etc/init.d/01dementedbomb:system/etc/init.d/01dementedbomb \
     vendor/DEMENTED-Droid/proprietary/common/etc/init.d/02dementedkernel:system/etc/init.d/02dementedkernel \
-    vendor/DEMENTED-Droid/proprietary/common/etc/init.d/05dementedrfs:system/etc/init.d/05dementedrfs \
+    vendor/DEMENTED-Droid/proprietary/common/etc/init.d/05dementedfs:system/etc/init.d/05dementedfs \
     vendor/DEMENTED-Droid/proprietary/common/etc/init.d/91dementedzipalign:system/etc/init.d/91dementedzipalign \
     vendor/DEMENTED-Droid/proprietary/common/etc/init.d/99dementedsystem:system/etc/init.d/99dementedsystem 
