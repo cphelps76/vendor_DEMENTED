27894f30c4edd572ae9c26974593535f5fbc0049
cphelps76 <cphelps76@gmail.com>
2013-06-24 16:59:12 -0500

    switchout init.d

--- config/speed_freek.mk
+++ config/speed_freek.mk
@@ -2,12 +2,16 @@
 #
 # init.d support
 PRODUCT_COPY_FILES += \
-    vendor/DEMENTED/prebuilt/demented/etc/init.d/01dementedbomb:system/etc/init.d/01dementedbomb \
-    vendor/DEMENTED/prebuilt/demented/etc/init.d/02dementedkernel:system/etc/init.d/02dementedkernel \
+    vendor/DEMENTED/prebuilt/demented/etc/init.d/01dementedkernel:system/etc/init.d/01dementedkernel \
+    vendor/DEMENTED/prebuilt/demented/etc/init.d/02sysctl:system/etc/init.d/02sysctl \
+    vendor/DEMENTED/prebuilt/demented/etc/init.d/04dementededt_tweaks:system/etc/init.d/04dementededt_tweaks \
     vendor/DEMENTED/prebuilt/demented/etc/init.d/05dementedfs:system/etc/init.d/05dementedfs \
+    vendor/DEMENTED/prebuilt/demented/etc/init.d/80dementedloopysmoothnesstweak:system/etc/init.d/80dementedloopysmoothnesstweak \
     vendor/DEMENTED/prebuilt/demented/etc/init.d/91dementedzipalign:system/etc/init.d/91dementedzipalign \
+    vendor/DEMENTED/prebuilt/demented/etc/init.d/97dementedtcp:system/etc/init.d/97dementedtcp \
     vendor/DEMENTED/prebuilt/demented/etc/init.d/99dementedsystem:system/etc/init.d/99dementedsystem \
-    vendor/DEMENTED/prebuilt/demented/etc/init.d/movecache:system/etc/init.d/movecache
+    vendor/DEMENTED/prebuilt/demented/etc/init.d/movecache:system/etc/init.d/movecache \
+    vendor/DEMENTED/prebuilt/demented/etc/init.d/98tweaks:system/etc/init.d/movecache
 
 # sysctl.conf
 PRODUCT_COPY_FILES += \
