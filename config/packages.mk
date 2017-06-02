1a457abd23102bb9d151126a2fca640b04b54bec
cphelps76 <cphelps76@gmail.com>
2017-06-01 21:42:10 -0500

    fix VCam2

--- config/packages.mk
+++ config/packages.mk
@@ -4,10 +4,11 @@ PRODUCT_PACKAGES += \
     Launcher3 \
     Apollo
 
-# Apps for car integration
+# Hostmode control apps
 PRODUCT_COPY_FILES += \
     vendor/demented/prebuilt/app/PowerEventMgr.apk:system/app/PowerEventMgr/PowerEventMgr.apk \
-    vendor/demented/prebuilt/app/VCam2.apk:system/app/VCam2/VCam2.apk
+    vendor/demented/prebuilt/app/VCam2.apk:system/app/VCam2/VCam2.apk \
+    vendor/demented/prebuilt/lib/libImageProc.so:system/lib/libImageProc.so
 
 # Misc for hostmode
 PRODUCT_COPY_FILES += \
