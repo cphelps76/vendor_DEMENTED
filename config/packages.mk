b6fc839ff5b901c52e23934f2888a5d2d795c9aa
cphelps76 <cphelps76@gmail.com>
2017-06-16 17:01:11 -0500

    update packages.mk

--- config/packages.mk
+++ config/packages.mk
@@ -1,16 +1,16 @@
 # Additional packages
 PRODUCT_PACKAGES += \
     AudioFX \
-    Launcher3 \
     Apollo
 
-# Hostmode control apps
+# Hostmode control apps for Timur's kernel
 PRODUCT_COPY_FILES += \
+    vendor/demented/prebuilt/app/app-release.apk:system/app/CarHome/CarHome.apk \
     vendor/demented/prebuilt/app/PowerEventMgr.apk:system/app/PowerEventMgr/PowerEventMgr.apk \
     vendor/demented/prebuilt/app/VCam2.apk:system/app/VCam2/VCam2.apk \
     vendor/demented/prebuilt/lib/libImageProc.so:system/lib/libImageProc.so
 
-# Misc for hostmode
+# Misc for hostmode for Timur's kernel
 PRODUCT_COPY_FILES += \
     vendor/demented/prebuilt/framework/services.jar:system/framework/services.jar \
     vendor/demented/prebuilt/system/powerdown.jpg:system/powerdown.jpg \
